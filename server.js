cef74223db7db65c5937bd5806babd24d477ff55
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-15 12:25:07 +0100

    DB setup

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
-
+const knex = require('knex');
 const server = express();
 
+// const db = require('./data/dbConfig.js');
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/budget.db3',
+  },
+  useNullAsDefault: true,
+});
+
+// DB helper functions here
+
 server.use(express.json());
+
+// Endpoint
 
 module.exports = server;
